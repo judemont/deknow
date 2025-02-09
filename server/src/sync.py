e1a0c03508565611a7cc6ec85279218bcf499459
JdM <julien@rmbi.ch>
2025-02-09 13:38:42 +0100

    Remove commented-out sync method and related logic from sync class

--- server/src/sync.py
+++ server/src/sync.py
@@ -6,30 +6,30 @@ class sync:
         self.friends = friends
         self.db_path = db_path
     
-    def sync(self):
-        db = Database(self.db_path)
+    # def sync(self):
+    #     db = Database(self.db_path)
 
-        print("Syncing DB with friends")
+    #     print("Syncing DB with friends")
 
-        i=0
-        for friend in self.friends:
-            print(f"Syncing DB with {friend}")
-            print(f"{i}/{len(self.friends)}")
-            response = requests.get(f"{friend}/pages")
-            pages = response.json()
+    #     i=0
+    #     for friend in self.friends:
+    #         print(f"Syncing DB with {friend}")
+    #         print(f"{i}/{len(self.friends)}")
+    #         response = requests.get(f"{friend}/pages")
+    #         pages = response.json()
 
-            for page in pages:
-                title, content, last_updated = page["title"], page["content"], page["last_updated"]
-                matching_pages = db.get_page(title=title)
+    #         for page in pages:
+    #             title, content, last_updated = page["title"], page["content"], page["last_updated"]
+    #             matching_pages = db.get_page(title=title)
 
-                if len(matching_pages) == 0:    
-                    db.new_page(title, content)
-                else:
-                    if last_updated > matching_pages[0][3]:
-                        db.update_page(matching_pages[0][0], title, content)
-            i+=1
+    #             if len(matching_pages) == 0:    
+    #                 db.new_page(title, content)
+    #             else:
+    #                 if last_updated > matching_pages[0][3]:
+    #                     db.update_page(matching_pages[0][0], title, content)
+    #         i+=1
 
-        print("Sync complete")
+    #     print("Sync complete")
 
             
             
